6b99a47b11a9c39ac0ce6f6f6aadcc6a01422325
Andrew <129807999+TheAndrewDang@users.noreply.github.com>
2025-01-04 22:10:22 -0800

    Update

--- frontend/src/app/components/project.tsx
+++ frontend/src/app/components/project.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
-import { motion } from 'framer-motion'
+import React from 'react';
+import { motion } from 'framer-motion';
 
 const project = () => {
-
   const fadeInVariants = {
     hidden: { opacity: 0, y: 20 },
     visible: (i: number) => ({
@@ -18,33 +17,32 @@ const project = () => {
 
   return (
     <div>
-          <div className="w-full flex flex-col items-center p-6 mt-8">
-            <div className="w-full max-w-7xl text-left">
-              <motion.h1
-                className="font-bold text-neutral-800 text-4xl md:text-5xl lg:text-6xl"
-                initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true }}
-                custom={0}
-                variants={fadeInVariants}
-              >
-                Projects
-              </motion.h1>
-              <motion.p
-                className="text-lg md:text-2xl lg:text-2xl font-semibold text-neutral-400"
-                initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true }}
-                custom={1}
-                variants={fadeInVariants}
-              >
-                Things I've worked on by myself or with others
-              </motion.p>
-            </div>
-          </div>
-      
+      <div className="w-full flex flex-col items-center p-6 mt-8">
+        <div className="w-full max-w-7xl text-left">
+          <motion.h1
+            className="font-bold text-neutral-800 text-4xl md:text-5xl lg:text-6xl"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            custom={0}
+            variants={fadeInVariants}
+          >
+            Projects
+          </motion.h1>
+          <motion.p
+            className="text-lg md:text-2xl lg:text-2xl font-semibold text-neutral-400"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            custom={1}
+            variants={fadeInVariants}
+          >
+            Things I&apos;ve worked on by myself or with others
+          </motion.p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default project
+export default project;
